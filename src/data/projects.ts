4695355421e4288d668d59a1fd1bf9a68e309cb7
Kartik shriniwas poojary <136634853+kartik0323@users.noreply.github.com>
2024-09-21 22:52:28 +0530

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,14 +16,7 @@ export const PROJECT_SHOWCASE: ProjectShowcaseListItem[] = [
     index: 1,
     title: "Doubtvcet",
     href: "/projects",
-    tags: [
-      "React.js",
-      "Node.js",
-      "MongoDB",
-      "Express.js",
-      "Vercel",
-      "REST API",
-    ],
+    tags: ["React.js", "Node.js", "MongoDB", "Express.js", "Vercel", "REST API"],
     image: {
       LIGHT: "/images/projects/doubtvcet.webp",
       DARK: "/images/projects/doubtvcet.webp",
@@ -35,24 +28,15 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
   {
     name: "AI Virtual Mouse",
     favicon: "/images/projects/logos/ML.ico",
-    imageUrl: [
-      "/images/projects/volumecontrol.webp",
-      "/images/projects/volumecontrol.webp",
-    ],
-    description:
-      "A virtual mouse that uses hand gestures to control your PC.",
-    sourceCodeHref:
-      "https://github.com/kartik2303/virtual-mouse-using-hand-gestures",
+    imageUrl: ["/images/projects/volumecontrol.webp", "/images/projects/volumecontrol.webp"],
+    description: "A virtual mouse that uses hand gestures to control your PC.",
+    sourceCodeHref: "https://github.com/kartik2303/virtual-mouse-using-hand-gestures",
   },
   {
     name: "Doubtvcet",
     favicon: "/images/projects/logos/reactjs.ico",
-    imageUrl: [
-      "/images/projects/doubtvcet.webp",
-      "/images/projects/doubtvcet.webp",
-    ],
-    description:
-      "A website designed for students who are shy to ask doubts in front of the whole class.",
+    imageUrl: ["/images/projects/doubtvcet.webp", "/images/projects/doubtvcet.webp"],
+    description: "A website designed for students who are shy to ask doubts in front of the whole class.",
     sourceCodeHref: "https://github.com/kartik0323/doubtvcet12",
     liveWebsiteHref: "https://doubtvcet.me",
   },
@@ -60,8 +44,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     name: "My Portfolio",
     favicon: "/favicon.ico",
     imageUrl: ["/static/homepage.png"],
-    description:
-      "My personal portfolio website built using Next.js, Tailwind CSS, and Framer Motion.",
+    description: "My personal portfolio website built using Next.js, Tailwind CSS, and Framer Motion.",
     sourceCodeHref: "https://github.com/kartik0323/kartikportfolio23",
     liveWebsiteHref: "https://kartikpoojary.tech",
   },
